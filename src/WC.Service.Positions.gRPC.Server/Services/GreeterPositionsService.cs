2e49544caa5a1a261f9b4f0c098e0a1545410038
SaintEfim <saintefim2@gmail.com>
2024-07-02 19:29:55 +0300

    update mapping

--- src/WC.Service.Positions.gRPC.Server/Services/GreeterPositionsService.cs
+++ src/WC.Service.Positions.gRPC.Server/Services/GreeterPositionsService.cs
@@ -1,4 +1,3 @@
-using AutoMapper;
 using Grpc.Core;
 using WC.Service.Positions.Domain.Abstractions.Models;
 using WC.Service.Positions.Domain.Abstractions.Services;
@@ -8,18 +7,19 @@ namespace WC.Service.Positions.gRPC.Server.Services;
 public class GreeterPositionsService : GreeterPositions.GreeterPositionsBase
 {
     private readonly IPositionProvider _provider;
-    private readonly IMapper _mapper;
 
-    public GreeterPositionsService(IPositionProvider provider, IMapper mapper)
+    public GreeterPositionsService(IPositionProvider provider)
     {
         _provider = provider;
-        _mapper = mapper;
     }
 
     public override async Task<CheckPositionResponse> CheckPositionExists(CheckPositionRequest request,
         ServerCallContext context)
     {
-        var result = await _provider.CheckPosition(_mapper.Map<PositionModel>(request));
+        var result = await _provider.CheckPosition(new PositionModel
+        {
+            Name = request.Position.Name
+        });
 
         return new CheckPositionResponse
         {
